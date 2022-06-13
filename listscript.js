5b287bb8ef489c736298e4166f6a36b79e3daa49
Luchito37 <lucamatteotti01@gmail.com>
2022-06-13 16:08:58 +0200

    semi-completato

---
+++ listscript.js
@@ -0,0 +1,31 @@
+new Vue ({
+    el: "#app",
+    data: {
+        listaSpesa: [
+            {
+                text: "Comprare le patate",
+                done: false,
+            },
+            {
+                text: "Comprare le banane",
+                done: false,
+            },
+            {
+                text: "Comprare i piatti",
+                done: false,
+            },
+            {
+                text: "Comprare le patate",
+                done: false,
+            },
+            {
+                text: "Comprare il detersivo",
+                done: false,
+            },
+            {
+                text: "Comprare il mangiare per i cani",
+                done: false,
+            },
+        ],
+    },
+});
